55e8792f0528073599b4c9f76b1da04ef74c6228
Snehasis Debbarman <snehasisdebbarman2016@gmail.com>
2024-01-26 07:11:45 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ const regex = require("./index");
 
 console.log(regex.creditCard.visa.test("4111111111111111")); // true
 console.log("majak")
+console.log("double iteration fixation of natural sign language flowis gixed now")
